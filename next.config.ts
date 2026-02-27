71227a6f450521810f58d2b9713ebbc6613b6f6b
TunaOcal <tunaocaalclear>
2026-02-27 16:24:36 +0300

    imp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/max-ai",
+        destination: "https://apps.apple.com/us/app/potential-analyzer-ai/id6757166626?ppid=0f0ffa50-24d6-4cc6-b026-7654298a6906",
+        permanent: false,
+      },
+      {
+        source: "/wardrobe-ai",
+        destination: "https://apps.apple.com/us/app/wardrobe-ai-outfit-maker/id6752615624?ppid=b667d076-f3bd-4a28-a832-2868ad586112",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
